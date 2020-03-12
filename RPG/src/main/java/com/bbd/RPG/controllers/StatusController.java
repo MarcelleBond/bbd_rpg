fedc7bc588c72723df2476fbbb9ae7aa2b9fbb9c
Dane Horn <dane.horn7915@gmail.com>
2020-03-12 11:46:27 +0200

    add what was lost in merge

--- RPG/src/main/java/com/bbd/RPG/controllers/StatusController.java
+++ RPG/src/main/java/com/bbd/RPG/controllers/StatusController.java
@@ -1,8 +1,7 @@
 package com.bbd.RPG.controllers;
 
 import com.bbd.RPG.RpgApplication;
-import com.bbd.RPG.models.Player;
-import com.bbd.RPG.models.Position;
+import com.bbd.RPG.models.*;
 import com.bbd.RPG.models.stompmessages.InitializeMapIn;
 import com.bbd.RPG.models.stompmessages.StatusOut;
 import com.bbd.RPG.models.stompmessages.StatusIn;
@@ -13,10 +12,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Controller
 public class StatusController {
@@ -34,6 +30,48 @@ public class StatusController {
 
         RpgApplication.game.map = map;
         RpgApplication.game.player = new Player(playerName, playerPos);
+
+        int avaliableSpaces = 0;
+        for (Character[] characters : map) {
+            for (Character chars : characters) {
+                if (chars == '0')
+                    avaliableSpaces++;
+            }
+        }
+
+        // find avaliable space and add enemies to the map
+        int enemyAmount = (avaliableSpaces > 10) ? new Random().nextInt((int)(Math.ceil(avaliableSpaces / 10) + 1)) : 1; //new Random().nextInt((int)(Math.ceil(map.length / 3) + 1)) ;
+        avaliableSpaces -= enemyAmount;
+        System.out.println(enemyAmount);
+        while (enemyAmount >= 0) {
+            int xPosition = new Random().nextInt(map.length) ;
+            int yPosition = new Random().nextInt(map.length) ;
+
+            if (map[xPosition][yPosition] == '0')
+            {
+                Position position = new Position(xPosition, yPosition);
+                Enemy enemy = EnemyFactory.newEnemy(position);
+                if (enemy != null)
+                    RpgApplication.game.enemies.add(enemy);
+                enemyAmount--;
+            }
+        }
+
+        // find avaliable space add items to the map
+        int itemAmount = (avaliableSpaces > 10) ? new Random().nextInt((int)(Math.ceil(avaliableSpaces / 10) + 1)) : 1; //new Random().nextInt((int)(Math.ceil(map.length / 3) + 1)) ;
+        while (itemAmount >= 0) {
+            int xPosition = new Random().nextInt(map.length) ;
+            int yPosition = new Random().nextInt(map.length) ;
+
+            if (map[xPosition][yPosition] == '0')
+            {
+                Position position = new Position(xPosition, yPosition);
+                Item item = ItemFactory.newItem(position);
+                if (item != null)
+                    RpgApplication.game.items.add(item);
+                itemAmount--;
+            }
+        }
 
         result.put("map", map);
         result.put("player", playerPos);
